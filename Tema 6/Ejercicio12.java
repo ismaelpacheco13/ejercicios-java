c469ed3a8f1677a1a4ada96dca4aa0f2df4a5ecb
Ismael Pacheco Torrecilla <ismaelpacheco13001@gmail.com>
2019-10-28 09:30:19 +0100

    Mejorado Ejercicio 12

--- Tema 6/Ejercicio12.java
+++ Tema 6/Ejercicio12.java
@@ -9,8 +9,6 @@ public class Ejercicio12 {
     int numeroAleatorio;
     char caracterAleatorio;
     
-    
-    
     for (int i = 1; i <= 500; i++) {
       System.out.print("\033[32m");
       for (int j = 1; j <= 80; j++) {
