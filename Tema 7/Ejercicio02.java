517217fbe6099bbffed96b605279a769e76c928c
Ismael Pacheco Torrecilla <ismaelpacheco13001@gmail.com>
2019-11-07 09:28:17 +0100

    Actualizado Tema 7

---
+++ Tema 7/Ejercicio02.java
@@ -0,0 +1,19 @@
+
+public class Ejercicio02 {
+
+  public static void main(String[] args) {
+    
+    String [] simbolo = new String [10];
+    simbolo [0] = "'a'";
+    simbolo [1] = "'x'";
+    simbolo [4] = "'@'";
+    simbolo [6] = "' '";
+    simbolo [7] = "'+'";
+    simbolo [8] = "'Q'";
+    
+    for (int i = 0; i < 10; i++) {
+      System.out.print(simbolo[i] + " ");
+    }
+  }
+  
+}
