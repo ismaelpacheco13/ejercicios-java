8f1617012ac0fc29be280bf38b0e084b62ba9286
Ismael Pacheco Torrecilla <ismaelpacheco13001@gmail.com>
2019-11-28 19:03:38 +0100

    Actualizado Tema 8

---
+++ Tema 8/Ejercicio20hasta28.java
@@ -0,0 +1,127 @@
+
+package matematicas;
+
+public class Ejercicio20hasta28 {
+  
+  // Muestra por pantalla todos los números de un array de números enteros
+  public static void muestraArrayInt(int x[]) {
+    for (int n : x) {
+      System.out.print(n + " ");
+    }
+    System.out.println();
+  }
+  
+  // Crea un array de tamaño n y los reyena con números aleatorios dentro de un rango pedido al usuario
+  public static int[] generaArrayInt(int n, int minimo, int maximo) {
+    int[] x = new int[n];
+
+    for(int i = 0; i < n; i++) {
+      x[i] = (int)(Math.random()*(maximo - minimo + 1) + minimo);
+    }
+      
+    return x;
+  }
+
+  // Devuelve el mínimo del array que se pasa como parámetro
+  public static int minimoArrayInt(int[] x) {
+    int minimo = Integer.MAX_VALUE;
+    
+    for (int n : x) {
+      if (n < minimo) {
+        minimo = n;
+      }
+    }
+    return minimo;
+  }
+  
+  // Devuelve el máximo del array que se pasa como parámetro
+  public static int maximoArrayInt(int[] x) {
+    int maximo = Integer.MIN_VALUE;
+    
+    for (int n : x) {
+      if (n > maximo) {
+        maximo = n;
+      }
+    }
+      
+    return maximo;
+  }
+  
+  // Función que devuelve la media de los números del array que se pasa como parámetro
+  public static double mediaArrayInt(int[] x) {
+    int suma = 0;
+    
+    for (int n : x) {
+      suma += n;
+    }
+      
+    return (double)suma / x.length;
+  }
+  
+  // Función que dice si un número está dentro o no de un array
+  public static boolean estaEnArrayInt(int[] x, int n) {
+    for (int numero : x) {
+      if (numero == n) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  // Función que busca un número en un array y devuelve la posición (índice) en la que se encuentra
+  public static int posicionEnArrayInt(int[] x, int n) {
+    for(int i = 0; i < x.length; i++) {
+      if (x[i] == n) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+  // Función que voltea un array de enteros
+  public static int[] volteaArrayInt(int[] x) {
+    int[] a = new int[x.length];
+    
+    for(int i = 0; i < x.length; i++) {
+      a[x.length - i - 1] = x[i];
+    }
+      
+    return a;
+  }
+  
+  // Función que rota n posiciones a la derecha los números de un array
+  public static int[] rotaDerechaArrayInt(int[] x, int n) {
+    
+    int[] a = x.clone(); // clona en a el contenido de x
+    int i;
+    int aux;
+    
+    while (n-- > 0) {
+      aux = a[a.length - 1];
+      for(i = a.length - 1; i > 0; i--) {
+        a[i] = a[i - 1];
+      }
+      a[0] = aux;
+    }
+      
+    return a;
+  }
+  
+  // Función que rota n posiciones a la izquierda los números de un array
+  public static int[] rotaIzquierdaArrayInt(int[] x, int n) {
+    
+    int[] a = x.clone(); // clona en a el contenido de x
+    int i;
+    int aux;
+    
+    while (n-- > 0) {
+      aux = a[0];
+      for(i = 0; i < a.length - 1; i++) {
+        a[i] = a[i + 1];
+      }
+      a[a.length - 1] = aux;
+    }
+      
+    return a;
+  }
+}
